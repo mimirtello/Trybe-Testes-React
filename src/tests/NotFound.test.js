54f61db091be80a78523c8501a67830bc8c8600b
Yasmin Reis Tello <mimirt08@gmail.com>
2022-08-16 19:26:37 -0300

    Requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+// src/renderWithRouter.js
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+};
+
+test('Teste se a página contém o texto Page requested not found', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/naoexiste');
+  const notFound = screen.getByRole('heading', {
+    name: /Page requested not found/i,
+    level: 2 });
+  const imgNotFound = screen.getByRole('img', {
+    name: 'Pikachu crying because the page requested was not found' });
+  expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  expect(notFound).toBeInTheDocument();
+});
